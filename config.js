8fe5a2bf5c0307e34329aa3da3450a9d356200dc
Cesar-Moran <cesarmorangithub@gmail.com>
2023-10-13 11:29:57 -0500

    Edited db_host

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        
     }
 }
